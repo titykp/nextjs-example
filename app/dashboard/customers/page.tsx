048b7f51d355afa78fbb4cebb56175ba6a088666
TiTY <tee25450@gmail.com>
2024-08-26 22:59:36 +0700

    feat: add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 export default function customers() {
   return <div>customers</div>;
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,12 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+};
 export const experimental_ppr = true;
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
